6c91b9d8f7d77f876bbc80f56facfee507aa6218
Nwadinigwe Victor <victorchiemerie02@gmail.com>
2023-12-16 10:54:04 +0100

    complete circle scroll layout

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the vertical bar element
-    var verticalBar = document.getElementById("vertical-bar");
+    // Get all circle elements
+    var circles = document.querySelectorAll(".circle");
 
     // Listen for the scroll event on the window
     window.addEventListener("scroll", function() {
-        // Calculate the height of the vertical bar based on the scroll position
-        var scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
-        var scrolled = (window.scrollY / scrollHeight) * 100;
+        // Calculate the height of each section based on the number of circles
+        var sectionHeight = document.documentElement.scrollHeight / circles.length;
 
-        // Set the height of the vertical bar
-        verticalBar.style.height = scrolled + "%";
+        // Calculate the current section based on the scroll position
+        var currentSection = Math.floor(window.scrollY / sectionHeight);
+
+        // Check if the user has reached the bottom of the page
+        var isAtBottom = (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+
+        // Loop through each circle and update its background color
+        circles.forEach(function(circle, index) {
+            if (isAtBottom || index <= currentSection) {
+                circle.style.backgroundColor = "black"; /* Activate color for current and previous circles, or all circles if at the bottom */
+            } else {
+                circle.style.backgroundColor = "transparent"; /* Deactivate color for future circles */
+            }
+        });
     });
 });
